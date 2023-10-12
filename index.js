c0157e5c2c9dd63a1f1a4398fe80fd289304b3b4
Jesus Tapia <chucholuco@gmail.com>
2023-10-12 10:44:18 -0600

    adding variables

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const routes = require('./routes')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+require('dotenv').config({path: 'variables.env'})
 
 // Cors permite que un cliente se conecte a otro servidor para intercambio de recursos
 const cors = require('cors')
 
 // conectar mongo
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/restapis', {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true
 })
 
@@ -19,8 +20,21 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
+// Definir un dominio(s) para recibir las peticiones
+const whitelist = [process.env.FRONTEND_URL]
+const corsOptions = {
+    origin: (origin, callback) => {
+        const existe = whitelist.some(dominio => dominio === origin)
+        if (existe) {
+            callback(null, true)
+        } else {
+            callback(new Error('No permitido por CORS'))
+        }
+    }
+}
+
 // Habilitar cors
-app.use(cors())
+app.use(cors(corsOptions))
 
 // Rutas de la app
 app.use('/', routes())
--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ const auth = require('../middleware/auth')
 module.exports = function() {
 
     // Agrega nuevos clientes via POST
-    router.post('/clientes', clienteController.nuevoCliente)
+    router.post('/clientes', auth, clienteController.nuevoCliente)
 
     // Obtener todos los clientes
     router.get('/clientes', 
@@ -19,54 +19,57 @@ module.exports = function() {
         clienteController.mostrarClientes)
 
     // muestra un cliente en especifico
-    router.get('/clientes/:idCliente', clienteController.mostrarCliente)
+    router.get('/clientes/:idCliente', auth, clienteController.mostrarCliente)
 
     // actualizar cliente
-    router.put('/clientes/:idCliente', clienteController.actualizarCliente)
+    router.put('/clientes/:idCliente',auth, clienteController.actualizarCliente)
 
     // eliminar cliente
-    router.delete('/clientes/:idCliente', clienteController.eliminarCliente)
+    router.delete('/clientes/:idCliente', auth, clienteController.eliminarCliente)
 
     /** Productos */
     // nuevos productos
     router.post('/productos', 
+        auth,
         productosController.subirArchivo,
         productosController.nuevoProducto
     );
 
     // Muestra todos los productos
-    router.get('/productos', productosController.mostrarProductos)
+    router.get('/productos', auth, productosController.mostrarProductos)
 
     // Muestra producto por id
-    router.get('/productos/:idProducto', productosController.mostrarProducto)
+    router.get('/productos/:idProducto', auth, productosController.mostrarProducto)
 
     // Actualizar producto
     router.put('/productos/:idProducto',
+        auth,
         productosController.subirArchivo,
         productosController.actualizarProducto)
 
     // Eliminar productos
     router.delete('/productos/:idProducto',
+        auth,
         productosController.eliminarProducto)
 
     // Busqueda de productos
-    router.post('/productos/busqueda/:query', productosController.buscarProducto)
+    router.post('/productos/busqueda/:query', auth, productosController.buscarProducto)
 
     /** Pedidos */
     // agrega nuevos pedidos
-    router.post('/pedidos/nuevo/:idUsuario', pedidosController.nuevoPedido)
+    router.post('/pedidos/nuevo/:idUsuario', auth, pedidosController.nuevoPedido)
 
     // mostrar todos los pedidos
-    router.get('/pedidos', pedidosController.mostrarPedidos)
+    router.get('/pedidos', auth, pedidosController.mostrarPedidos)
 
     // Mostrar un pedido por su id
-    router.get('/pedidos/:idPedido', pedidosController.mostrarPedido)
+    router.get('/pedidos/:idPedido', auth, pedidosController.mostrarPedido)
 
     // Actualizar pedido
-    router.put('/pedidos/:idPedido', pedidosController.actualizarPedido)
+    router.put('/pedidos/:idPedido', auth, pedidosController.actualizarPedido)
 
     // Eliminar pedido
-    router.delete('/pedidos/:idPedido', pedidosController.eliminarPedido)
+    router.delete('/pedidos/:idPedido', auth, pedidosController.eliminarPedido)
 
     // Usuarios
     router.post('/crear-cuenta', usuariosController.registrarUsuario)
